0c3ed8ccd5a6cbee420a4fad8a7c71717b96acec
Pwneill <phelan.oneill@gmail.com>
2021-05-25 10:21:32 -0500

    playing with positions

--- script.js
+++ script.js
@@ -476,7 +476,7 @@ var markerIcon = L.Icon.extend({
     }
 })
 
-var mymap = L.map('mapid').setView([41.81944, -87.327778], 9.9);
+var mymap = L.map('mapid').setView([41.81944, -87.7], 9.9);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
